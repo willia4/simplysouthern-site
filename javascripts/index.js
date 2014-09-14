234e329ee7bc5ad29fa8ac1bf4bc67a6c1205f07
willia4 <james@jameswilliams.me>
2014-09-14 14:21:35 -0400

    Add a ghetto coming soon watermark

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
+	setTimeout(function() {
+		$("#watermark-container").addClass("out");
+
+		setTimeout(function() {
+			$("#watermark-container").css({"display": "none"});
+		}, 2000);
+
+	}, 800);
 	// $(".carousel").carousel();
 });
